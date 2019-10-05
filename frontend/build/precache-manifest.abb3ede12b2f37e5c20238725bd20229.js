02bebcdabe18d4ef751e1ca8d1f61dbc9872f302
leo <leo.bravo.rain@gmail.com>
2019-10-05 13:50:16 -0300

    build version of react app. Init firebase and deploy hosting

---
+++ frontend/build/precache-manifest.abb3ede12b2f37e5c20238725bd20229.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373e00f8e30aa92a5b5ba731d99eea03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "611f6cad33b5b3d81c10",
+    "url": "/static/css/2.a65581f3.chunk.css"
+  },
+  {
+    "revision": "611f6cad33b5b3d81c10",
+    "url": "/static/js/2.307f9ef9.chunk.js"
+  },
+  {
+    "revision": "f5c4ff85836f115324db",
+    "url": "/static/js/main.a10e4cfd.chunk.js"
+  },
+  {
+    "revision": "219707186523ad4b8cde",
+    "url": "/static/js/runtime~main.821e3928.js"
+  }
+]);
