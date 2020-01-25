58cad83668d1da2216013364216f125a460f795d
leoBravoRain <leo.bravo.rain@gmail.com>
2020-01-25 18:06:23 -0500

    fix image background of wp applicaciont (I stored the .json file of template)

---
+++ frontend/build/precache-manifest.0d33aa8a62c776afcb7dd0dee3516ad2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b017201caae8ea3aac84c9d661ef23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b23c5d6a4653cfa8111d",
+    "url": "/static/css/2.c9194dd8.chunk.css"
+  },
+  {
+    "revision": "b23c5d6a4653cfa8111d",
+    "url": "/static/js/2.f4f1f007.chunk.js"
+  },
+  {
+    "revision": "5b2cae3609155ea3d644b86c22417f9b",
+    "url": "/static/js/2.f4f1f007.chunk.js.LICENSE"
+  },
+  {
+    "revision": "282c0bb491079626b9a7",
+    "url": "/static/js/main.c0e88e4a.chunk.js"
+  },
+  {
+    "revision": "b5ff61001688404db26f",
+    "url": "/static/js/runtime-main.8c5f780f.js"
+  },
+  {
+    "revision": "708b10eec321c3dc56f52d6cddd18076",
+    "url": "/static/media/2_Yo.708b10ee.png"
+  },
+  {
+    "revision": "4c0119c810ba633493edbc4954be4fa5",
+    "url": "/static/media/logo.4c0119c8.jpg"
+  }
+]);
